e4afb0f4d609fc582f03b1c0b43520e58272568c
Mizchelle Rose Sabangan <145424260+Mizchelle@users.noreply.github.com>
2023-09-29 15:44:04 -0800

    first fommit

---
+++ Controllers/HomeControllerrr.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        var studentInfoArray = new[]
+        {
+            new StudentInfoModel { Name = "Mizchelle Rose Sabangan", StudentId = "S001", StudentAge = "21" },
+            new StudentInfoModel { Name = "Roselle Mae Dellomas", StudentId = "S002", StudentAge = "22" },
+            new StudentInfoModel { Name = "Milby Dellomas", StudentId = "S003", StudentAge = "20" }
+        };
+
+        return View(studentInfoArray);
+    }
+}
